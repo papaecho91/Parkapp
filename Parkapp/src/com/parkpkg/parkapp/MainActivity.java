c3db35fd1fc38b085d850fffc309eedfc277113e
Patrik <Patrik@Patrik-Swagger>
2014-07-15 17:40:51 +0100

    Added methods to check googleplay and googlemaps connection

--- Parkapp/src/com/parkpkg/parkapp/MainActivity.java
+++ Parkapp/src/com/parkpkg/parkapp/MainActivity.java
@@ -1,21 +1,53 @@
 package com.parkpkg.parkapp;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	private GoogleMap mMap;
-
 	
-//
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         
+        
+        if(connectionToGooglePlay()){
+        	setContentView(R.layout.activity_main);
+        }
+        setUpMapIfNeeded();
     }
+    
+    // Testing if googleplay services is working
+    private boolean connectionToGooglePlay(){
+    	int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+    	if(status == ConnectionResult.SUCCESS){
+    		return true;
+    	}else{
+    		Toast.makeText(this, "Google play is not available", Toast.LENGTH_SHORT).show();
+    	}return false;
+    		
+    		
+    }
+    
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                // The Map is verified. It is now safe to manipulate the map.
+
+            }
+        }
+    }
+    
 
 }
