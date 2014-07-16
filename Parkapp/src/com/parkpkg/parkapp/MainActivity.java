a2ce3e12cb138c2f44dae196e7fc65f1b8e4ceae
Patrik <Patrik@192.168.1.105>
2014-07-16 16:45:43 +0100

    Added Göteborg to location, only grid

--- Parkapp/src/com/parkpkg/parkapp/MainActivity.java
+++ Parkapp/src/com/parkpkg/parkapp/MainActivity.java
@@ -1,17 +1,39 @@
+/**
+ * MainActivity.java 2014-07-16
+ * Copyright Patrik Evertsson
+ */
+
 package com.parkpkg.parkapp;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+/**
+ * 
+ * @author Patrik Evertsson
+ * @version 0.0.2
+ *
+ */
+
+public class MainActivity extends Activity implements LocationListener{
+
+	// instance variables
+
+	private GoogleMap googleMap;
 	
-	private GoogleMap mMap;
 	
 	
     @Override
@@ -21,33 +43,74 @@ public class MainActivity extends Activity {
         
         if(connectionToGooglePlay()){
         	setContentView(R.layout.activity_main);
+        	setUpMapIfNeeded();
         }
-        setUpMapIfNeeded();
+        
     }
-    
-    // Testing if googleplay services is working
+    /**
+     * Test connection between the app and Google Play
+     * @return true if the connection works, false it displays a dialog with the error
+     */
     private boolean connectionToGooglePlay(){
     	int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
     	if(status == ConnectionResult.SUCCESS){
     		return true;
     	}else{
-    		Toast.makeText(this, "Google play is not available", Toast.LENGTH_SHORT).show();
+    		((Dialog) GooglePlayServicesUtil.getErrorDialog(status,this,10)).show();
     	}return false;
     		
     		
     }
-    
+    /**
+     * This method is used to make sure we can interact and customize the map
+     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            
             // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                // The Map is verified. It is now safe to manipulate the map.
+            if (googleMap != null) {
+                
+            	googleMap.setMyLocationEnabled(true);
+            	LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+            	String provider = lm.getBestProvider(new Criteria(), true);
+            	
+            	if (provider == null) {
+            		onProviderDisabled(provider);
+            	}
+            	
+            	Location loc = lm.getLastKnownLocation(provider);
+            	if(loc != null){
+            		onLocationChanged(loc);
+            	}
 
             }
         }
     }
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		LatLng latLng = new LatLng(57.7000, 11.9667);
+		
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+		googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+	}
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
+	}
     
 
 }
